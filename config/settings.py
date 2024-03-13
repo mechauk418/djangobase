e16341cd162b8e55f2829b50b3f993cdf0970309
mechauk418 <mechauk418@gmail.com>
2024-03-13 10:27:50 +0900

    CICD16

--- config/settings.py
+++ config/settings.py
@@ -168,8 +168,6 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://sub.example.com",
     "http://localhost:8080",
     "http://127.0.0.1:8080",
     "http://127.0.0.1:9000",
