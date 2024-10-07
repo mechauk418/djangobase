76b3f1e5a06b20bcb9ccd5c914a37979a3d81829
mechauk418 <mechauk418@gmail.com>
2024-10-07 16:34:35 +0900

    clientip03

--- djangobase2/article/views.py
+++ djangobase2/article/views.py
@@ -190,11 +190,11 @@ import os
 def envview(request):
 
     def get_client_ip(request):
-        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-        if x_forwarded_for:
-            ip = x_forwarded_for.split(',')[0]
-        else:
-            ip = request.META.get('REMOTE_ADDR')
+        # x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        # if x_forwarded_for:
+        #     ip = x_forwarded_for.split(',')[0]
+        # else:
+        ip = request.META.get('REMOTE_ADDR')
         return ip
 
     testdict = {
