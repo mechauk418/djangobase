d134cbb391fd7973468389743d6b1a1a2756f755
mechauk418 <mechauk418@gmail.com>
2024-12-04 23:36:44 +0900

    1204

--- djangobase2/config/handlers.py
+++ djangobase2/config/handlers.py
@@ -31,29 +31,3 @@ class S3RotatingFileHandler(logging.handlers.RotatingFileHandler):
     def doRollover(self):
         super().doRollover()
         log_file_name = os.path.basename(self.baseFilename)
-
-
-from storages.backends.s3boto3 import S3Boto3Storage
-
-class S3LoggingHandler(logging.Handler):
-    def __init__(self, bucket_name, log_file_prefix):
-        logging.Handler.__init__(self)
-        self.s3_client = S3Boto3Storage(bucket_name=bucket_name)
-        self.log_file_prefix = log_file_prefix
-
-    def emit(self, record):
-        log_entry = self.format(record)
-        # Implement your logic for rotating file names or managing versions here
-        file_name = f"{self.log_file_prefix}.log"
-        self.upload_log_to_s3(file_name, log_entry)
-
-    def upload_log_to_s3(self, file_name, log_entry):
-        # Append to the file if it exists, else create a new file
-        try:
-            # Try to read the existing log file from S3
-            content = self.s3_client.open(file_name).read()
-            new_content = content + '\n' + log_entry
-        except:
-            new_content = log_entry
-        # Write back the updated content
-        self.s3_client.save(file_name, new_content)
